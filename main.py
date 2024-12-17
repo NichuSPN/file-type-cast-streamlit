8477d6444643e8df5fd6440460cd41c2f671d0d2
Nishaanth S P <nishaanth.sp@swymcorp.com>
2024-12-17 21:30:08 +0530

    remove unwanted fragments

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import streamlit as st
 
-@st.fragment
-def singlepage():
-    return st.Page("singlepage.py", title="Single Page")
+st.Page("singlepage.py", title="Single Page")
 
-@st.fragment
-def tabbed():
-    return st.Page("tabs.py", title="Tabbed Interface")
+st.Page("tabs.py", title="Tabbed Interface")
 pages = st.navigation([
-    singlepage(),
-    tabbed()])
+    st.Page("singlepage.py", title="Single Page"),
+    st.Page("tabs.py", title="Tabbed Interface")])
 
 pages.run()
